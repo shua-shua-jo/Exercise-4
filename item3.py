c339f719850bc7cc22fdade230a0b3b7142becc5
Josh <joshuaabello02@gmail.com>
2022-11-18 23:54:14 +0800

    update documentation for item3

--- item3.py
+++ item3.py
@@ -6,15 +6,56 @@ import matplotlib.pyplot as plt
 plt.style.use('seaborn-v0_8-pastel')
 
 def f(x):
+    """
+    Polynomial Function
+
+    Args:
+        x : vector
+
+    Returns:
+        f : real number
+            function value at x
+    """
     return 1 / (1 - x**2 + x**4)
 
 def Pn(z, a, b, deg):
+    """
+    Polynomial interpolation of f using Lagrange
+
+    Args:
+        z : vector
+        a : real number
+            lower bound
+        b : real number
+            upper bound
+        deg : integer
+            degree of polynomial
+
+    Returns:
+        Pz : vector
+            lagrange interpolation polynomial
+    """
     x = chebyshev(a, b, deg)
     fx = f(x)
     Pz = poly_eval.LagrangeInterpolation(fx, x, z)
     return Pz
 
 def chebyshev(a, b, n):
+    """
+    Zeros of the Chebyshev Polynomial
+
+    Args:
+        a : real number
+            lower bound
+        b : real number
+            upper bound
+        n : integer
+            degree of polynomial
+
+    Returns:
+        x : vector
+            chebyshev nodes 
+    """
     x = np.empty(n)
     t1 = (a + b) / 2
     t2 = (b - a) / 2
@@ -22,6 +63,7 @@ def chebyshev(a, b, n):
         x[k] = t1 + t2 * math.cos((2 * k + 1) * math.pi / (2 * n + 2))
     return x
 
+# Functions for supnorm for 3 intervals
 def g1(x, deg):
     return f(x) - Pn(x, -5, ub1, deg)
 def g2(x, deg):
@@ -62,13 +104,15 @@ Pz2 = Pn(z2, ub1, ub2, degree)
 print(f"supnorm at [{round(ub2,2)}, 5.00]\t\t: \t", sup3)
 Pz3 = Pn(z3, ub2, 5, degree)
 
-plt.rcParams['figure.figsize'] = [13, 7]
-
 print(f"supnorm = {supnorm} for 3 subintervals")
+
+# plotting the function and polynomial interpolation
+
+plt.rcParams['figure.figsize'] = [13, 7]
 
 fig, (ax1, ax2, ax3) = plt.subplots(1, 3, sharey=True)
 
-fig.suptitle('Function f and Lagrange Polynomial P_z in different subintervals')
+fig.suptitle('Function f and Lagrange Interpolation Polynomial P_z in different subintervals')
 ax1.plot(z1, fz1, 'k', z1, Pz1, 'b')
 ax1.set_title('[-5, -1.667]')
 ax2.plot(z2, fz2, 'k', z2, Pz2, 'b')
